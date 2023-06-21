8d005ca4a0b3f1fad5f02e88ba709b73babe874f
woojin Kim <woojkk@nvaer.com>
2023-06-21 13:18:26 +0900

    Refactor test : Refactored unit test
    통합 테스트를 유닛테스트로 변경

--- src/test/java/woojkk/fintechProject/service/AccountUserServiceTest.java
+++ src/test/java/woojkk/fintechProject/service/AccountUserServiceTest.java
@@ -1,33 +1,71 @@
 package woojkk.fintechProject.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.DisplayName;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import woojkk.fintechProject.domain.AccountUser;
 import woojkk.fintechProject.dto.LoginForm;
+import woojkk.fintechProject.repository.AccountUserRepository;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class AccountUserServiceTest {
-  @Autowired
+
+  @Mock
+  private AccountUserRepository accountUserRepository;
+
+  @InjectMocks
   private AccountUserService accountUserService;
 
   @Test
-  @DisplayName("로그인 성공")
-  void login() {
-    //given
-    LoginForm form = LoginForm.builder()
-        .email("woojkk@naver.com")
-        .password("6877")
-        .build();
+  void loginTest() {
+      //given
+    given(accountUserRepository.findByEmail(any()))
+        .willReturn(Optional.of(AccountUser.builder()
+                .name("rrr")
+                .email("test@naver.com")
+                .password("123")
+            .build()));
+    ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
 
-    AccountUser accountUser = accountUserService.login(form);
-    assertNotNull(accountUser.getEmail());
-    assertNotNull(accountUser.getPassword());
-    assertEquals("woojkk@naver.com", accountUser.getEmail());
-    assertEquals("6877", accountUser.getPassword());
+    //when
+    AccountUser accountUser = accountUserService.login(LoginForm.builder()
+            .email("abc@naver.com")
+            .password("456")
+        .build());
+      //then
+    verify(accountUserRepository, times(1)).findByEmail(captor.capture());
+    verify(accountUserRepository, times(0)).save(any());
+    assertEquals("abc@naver.com", captor.getValue());
+    assertNotEquals("abcd@naver.com", captor.getValue());
+    assertEquals("123", accountUser.getPassword());
+    assertEquals("rrr", accountUser.getName());
   }
+
+//
+//  @Test
+//  @DisplayName("로그인 성공")
+//  void login() {
+//    //given
+//    LoginForm form = LoginForm.builder()
+//        .email("abc@naver.com")
+//        .password("6877")
+//        .build();
+//
+//    AccountUser accountUser = accountUserService.login(form);
+//    assertNotNull(accountUser.getEmail());
+//    assertNotNull(accountUser.getPassword());
+//    assertEquals("abc@naver.com", accountUser.getEmail());
+//    assertEquals("4416", accountUser.getPassword());
+//  }
 }
