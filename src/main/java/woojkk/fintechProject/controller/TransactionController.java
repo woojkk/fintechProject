85f6b0f0e4e02094862556d872ded3ba2113cbce
woojin Kim <woojkk@nvaer.com>
2023-07-09 23:47:27 +0900

    Refactor : 입출금 기능 구현
    오타 수정 및 validateToken 메서드 응집, 중복코드 줄이기

--- src/main/java/woojkk/fintechProject/controller/TransactionController.java
+++ src/main/java/woojkk/fintechProject/controller/TransactionController.java
@@ -3,7 +3,6 @@ package woojkk.fintechProject.controller;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,7 @@ public class TransactionController {
 
   private final TransactionService transactionService;
 
-  @PutMapping("/transaction/withdraw")
+  @PostMapping("/transaction/withdraw")
   public TransactionDto.Response withdrawMoney(@RequestHeader(name = "X-AUTH-TOKEN") String token,
       @RequestBody @Valid TransactionDto.Request request) {
 
